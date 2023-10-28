5f9a121c185041e6b03ddde9f306095307d842f6
Plo4i <midag@abv.bg>
2023-10-29 00:02:14 +0200

    Added comments

--- SEP5 Source Code/public/scripts/main.js
+++ SEP5 Source Code/public/scripts/main.js
@@ -1,4 +1,8 @@
+// Add a scroll event listener to the window
 window.addEventListener('scroll', function () {
+    // Select the element with the class 'nav-container' and store it in the 'header' variable
     const header = document.querySelector('.nav-container');
+
+    // Toggle the 'sticky' class on the 'header' element based on the scroll position
     header.classList.toggle("sticky", window.scrollY > 0);
 });
