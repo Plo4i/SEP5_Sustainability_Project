f1a8f7efe1ebb4d05268d9c121a3a65b8a5ec3c7
sigurdHorsholt <sigurd-horsholt@hotmail.com>
2023-10-31 16:47:27 +0100

    packages installed, express, bodyparser, path

---
+++ SEP5 Source Code/routes/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { error } from 'console';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
+const router = express.Router();
+
+
+router.get('/', (req,res) => {
+    const options = path.join(__dirname, '../views/pages')
+    console.log("PATH: ")
+    console.log(options);
+
+    res.sendFile(options+'/index.html',(error) => {
+        console.log("")
+        console.log("error finding file" + error);
+    })
+
+})
+
+export default router;
