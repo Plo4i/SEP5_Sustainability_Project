27b3c91f98b28eaa52522bb30e0c1868e1b05803
Mar7inD <Dotev32@gmail.com>
2023-11-02 11:41:30 +0100

    Some file editing

---
+++ SEP5 Source Code/config/valueInput.sql
@@ -0,0 +1,7 @@
+SET search_path TO ecoeval;
+
+INSERT INTO companies  VALUES (31623197, 'Shell Denmark', 'https://www.shell.dk/',
+                               'info@shell.dk','/images/Shell-Card-Image.jpg', 'Oil and petrol');
+
+INSERT INTO companies  VALUES (39009323, 'Amazon Web Services', 'https://www.amazon.de/',
+                               'info@amazon.dk','/images/Amazon-Logo.png', 'Online shopping website');
