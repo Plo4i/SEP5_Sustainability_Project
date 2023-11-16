ad42e17a6e411aa6db25db467d4ed94a9abd76c2
Plo4i <midag@abv.bg>
2023-11-16 11:12:28 +0100

    Added a headerScript.js for the front-end of the header.ejs

--- SEP5 Source Code/public/scripts/main.js
+++ SEP5 Source Code/public/scripts/main.js
@@ -1,28 +1,3 @@
-// Add a scroll event listener to the window
-window.addEventListener("scroll", function () {
-  // Select the element with the class 'nav-container' and store it in the 'header' variable
-  const header = document.querySelector(".nav-container");
-
-  // Toggle the 'sticky' class on the 'header' element based on the scroll position
-  header.classList.toggle("sticky-navbar", window.scrollY > 0);
-});
-
-// Serchbar functionality - event listener
-// Open searchbar
-document.getElementsByClassName("search-icon").addEventListener("click", function () {
-  var navLinks = document.getElementById("nav-links");
-  navLinks.classList.remove("nav-links-searchbar-off");
-  navLinks.classList.add("nav-links-searchbar-on");
-  document.getElementById("search-bar").style.display = "block";
-});
-
-document.getElementById("close-btn").addEventListener("click", function () {
-  var navLinks = document.getElementById("nav-links");
-  navLinks.classList.remove("nav-links-searchbar-on");
-  navLinks.classList.add("nav-links-searchbar-off");
-  document.getElementById("search-bar").style.display = "none";
-});
-
 //Log in modal
 
 //Getting login link in header
