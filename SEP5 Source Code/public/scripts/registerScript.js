2eb32b483e1012d2d64d868786e47f26d11ebb1b
Plo4i <midag@abv.bg>
2023-11-17 18:35:19 +0100

    Made additional styling and added show password toggle

--- SEP5 Source Code/public/scripts/registerScript.js
+++ SEP5 Source Code/public/scripts/registerScript.js
@@ -1,6 +1,9 @@
+// Declare variables
 var passwordInput = document.getElementById('password-input');
 var passwordContainer = document.querySelector('.password-container');
 
+// This script is toggling the bottom border of the "password-container" div to become orange
+// when the user inputs a password in both the register and login form
 passwordInput.addEventListener('focus', function () {
     passwordContainer.style.borderBottom = '3px solid var(--orange-button-color)';
 });
@@ -8,3 +11,17 @@ passwordInput.addEventListener('focus', function () {
 passwordInput.addEventListener('blur', function () {
     passwordContainer.style.borderBottom = '3px solid var(--white-font-color)';
 });
+
+// This function toggles the visibility of the password in both the register and login form
+function togglePasswordVisibility() {
+    var passwordInput = document.getElementById('password-input');
+    var icon = document.getElementById('toggle-password');
+
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        icon.setAttribute('data-icon', 'mdi:eye-off');
+    } else {
+        passwordInput.type = 'password';
+        icon.setAttribute('data-icon', 'mdi:eye');
+    }
+}
